be11d10b0095e907e2c2c416517c305bf4162142
Erik <ginshmastc@gmail.com>
2016-07-29 17:55:02 -0700

    Update CIF_module.js

--- Javascript/modules/CIF_module.js
+++ Javascript/modules/CIF_module.js
@@ -1,12 +1,17 @@
 /*
 This module contains functions and logic for the CIF volleybal book style.
 */
-//CSS file should be loaded here also if possible
-function getModule(module) {
+
+
+function getModule() {
 	//define functions and set common functions names to their declarations contained here
     
     var onStart = function ()
         {
+
+            var bgimage = "url('Images/layout2.png')";
+            document.getElementById("myBook").style.backgroundImage = bgimage;
+
             a_points = 0;
             a_teamName = "TeamA";
             a_liberoServeRotation = -1;//libero has not served yet
@@ -16,14 +21,14 @@ function getModule(module) {
             a_timeouts = 0;
 
             b_points = 0;
-            b_teamName = "TeamA";
+            b_teamName = "TeamB";
             b_liberoServeRotation = -1;//libero has not served yet
             b_setsWon = 0;
             b_rotationPosition = 0;
             b_subs = 0;
             b_timeouts = 0;
 
-            drawText(.15, .15, a_teamName);
+            drawText(.15, .05, a_teamName);
         };
     var onPoint = function (team)
         {
@@ -31,7 +36,7 @@ function getModule(module) {
             {
                 a_points++;
             }
-        }
+        };
     var onSubstitution = function (team, position, number)
         {
             
