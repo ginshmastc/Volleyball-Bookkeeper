ed660def7541e572e0817a295878c6b74e9a6971
Erik <ginshmastc@gmail.com>
2016-07-30 22:24:30 -0700

    Update MenuOverlay.js

--- Javascript/MenuOverlay.js
+++ Javascript/MenuOverlay.js
@@ -2,16 +2,6 @@
 This file contains the functions for the menu overlay that operates the book.
 */
 
-
-/*
-Prepares the canvas to be drawn on by storing the canvas object, context, width,
-and height as well as sets canvasReady to true.
-*/
-function init()
-{
-    
-}
-
 function openMenu() {
     document.getElementById("overlay").style.width = "100%";
 }
