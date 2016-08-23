13165fcf2758885503c0d1923fe63a5f53e194fe
Erik <ginshmastc@gmail.com>
2016-08-22 18:38:22 -0700

    Update CIF_module.js

--- Javascript/modules/CIF_module.js
+++ Javascript/modules/CIF_module.js
@@ -38,7 +38,7 @@ function getModule() {
         {
 
             var bgimage = "url('Images/CIF_image.png')";
-            document.getElementById("myBook").style.backgroundImage = bgimage;
+            //document.getElementById("myBook").style.backgroundImage = bgimage;
 
             a_points = 0;
             a_lineup = startingData.aLineup;
