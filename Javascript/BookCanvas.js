0a7b22c19defb897799bee61e23ddf1e78d353b5
Erik <ginshmastc@gmail.com>
2016-08-30 12:27:08 -0700

    Update BookCanvas.js

--- Javascript/BookCanvas.js
+++ Javascript/BookCanvas.js
@@ -38,8 +38,8 @@ function init()
     openMenu();
         }, false);
 
-    var test = '{"teamA":"TEAM A", "teamB":"TEAM B", "aLineup":[1,2,3,4,5,6,7], "bLineup":[7,6,5,4,3,2,1], "timeoutCap":2, "aServe":false, "module":"Javascript/modules/CIF_module.js"}';
-    setStartingData(test);
+    //var test = '{"teamA":"TEAM A", "teamB":"TEAM B", "aLineup":[1,2,3,4,5,6,7], "bLineup":[7,6,5,4,3,2,1], "timeoutCap":2, "aServe":false, "module":"Javascript/modules/CIF_module.js"}';
+    //setStartingData(test);
 }
 
 /*
