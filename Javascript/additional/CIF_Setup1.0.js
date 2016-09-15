eb8ecc702d81f495791b0f62c1f577e4658ace46
Erik <ginshmastc@gmail.com>
2016-09-15 14:42:28 -0700

    Update and rename CIF_Setup.js to CIF_Setup1.0.js

---
+++ Javascript/additional/CIF_Setup1.0.js
@@ -0,0 +1,25 @@
+/*
+This file gives functions to the CIF setup page.
+*/
+
+var lineupForm = "https://cdn.rawgit.com/ginshmastc/Volleyball-Bookkeeper/master/HTML/Lineups6p1l.html";
+
+
+function lineups()
+{
+    var saved = [];
+
+    saved[0] = document.getElementById('teamA').value + "";
+    saved[1] = document.getElementById('teamB').value + "";
+    saved[2] = document.getElementById('sets').value + "";
+    saved[3] = document.getElementById('playTo').value + "";
+    saved[4] = document.getElementById('cap').value + "";
+
+    if(Android != null)
+    {
+        Android.addData(saved);
+        Android.loadNextForm(lineupForm);
+    }
+
+}
+
