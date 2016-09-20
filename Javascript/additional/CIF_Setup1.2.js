d55e6c1406ffa6de54cb58a20df0c0cdc3394d4e
Erik <ginshmastc@gmail.com>
2016-09-20 14:22:13 -0700

    Update CIF_Setup1.2.js

--- Javascript/additional/CIF_Setup1.2.js
+++ Javascript/additional/CIF_Setup1.2.js
@@ -8,25 +8,16 @@ var lineupForm = "https://cdn.rawgit.com/ginshmastc/Volleyball-Bookkeeper/master
 function lineups()
 {
     var saved = [];
-/*
     saved[0] = document.getElementById('teamA').value + "";
     saved[1] = document.getElementById('teamB').value + "";
     saved[2] = document.getElementById('sets').value + "";
     saved[3] = document.getElementById('playTo').value + "";
     saved[4] = document.getElementById('cap').value + "";
-*/
-saved[0] = "a";
-    saved[1] = "b";
-    saved[2] = "c";
-    saved[3] = "d";
-    saved[4] = "e";
+
     if(Android != null)
     {
-        Android.test("test message");
-        Android.test(saved[0]);
         Android.addData(saved);
         Android.loadNextForm(lineupForm);
     }
-
 }
 
