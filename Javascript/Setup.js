c67e2386ca7f09ef04dbdf4edde0d2a6f5ed44f5
Erik <ginshmastc@gmail.com>
2016-09-22 18:12:54 -0700

    Update Setup.js

--- Javascript/Setup.js
+++ Javascript/Setup.js
@@ -1,13 +1,14 @@
 
 function loadUrl(newLocation)
 {
+  Android.test("loadURL" + newLocation);
   window.location = newLocation;
   return false;
 }
 
 function injectJS(path)
 {
-
+Android.test("injectJS " + path);
     var script = document.createElement('script');
     script.onload = function() {
       onStart();
