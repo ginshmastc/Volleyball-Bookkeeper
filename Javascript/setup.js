4ea5b14ed0d293d5eb7295478e1232efe99bddc2
Erik <ginshmastc@gmail.com>
2016-09-22 17:49:18 -0700

    Update setup.js

--- Javascript/setup.js
+++ Javascript/setup.js
@@ -4,3 +4,15 @@ function loadUrl(newLocation)
   window.location = newLocation;
   return false;
 }
+
+function injectJS(path)
+{
+
+    var script = document.createElement('script');
+    script.onload = function() {
+      onStart();
+    };
+    script.src = path;
+    document.getElementsByTagName('head')[0].appendChild(script);
+}
+
