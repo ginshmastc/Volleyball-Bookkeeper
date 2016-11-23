dfce95f486a7764174c95c858b2262a7bb3c431e
Erik <ginshmastc@gmail.com>
2016-11-22 17:42:46 -0800

    Update and rename CIF-setup2.7.js to CIF-setup2.8.js

--- Javascript/additional/CIF-setup2.8.js
+++ Javascript/additional/CIF-setup2.8.js
@@ -3,9 +3,17 @@ This file gives functions to the CIF setup page.
 */
 
 var saved;
+var teamA;
+var teamB;
+var lineupA;
+var lineupB;
+
+
 function init()
 {
     saved = "";
+    teamA = "";
+    teamB = "";
     openOverlay1();
 }
 
@@ -37,8 +45,38 @@ function openOverlay2()
     
     resetOverlays();
     document.getElementById('overlay2').style.width = "100%";
-    document.getElementById('ateamlabel').innerHTML = document.getElementById('teamA').value;
-    document.getElementById('bteamlabel').innerHTML = document.getElementById('teamB').value;
+    teamA = document.getElementById('teamA').value;
+    teamB = document.getElementById('teamB').value;
+    document.getElementById('ateamlabel').innerHTML = teamA;
+    document.getElementById('bteamlabel').innerHTML = teamB;
+}
+
+function openOverlay2(a_team, b_team, a_lineup, b_lineup)
+{   
+    resetOverlays();
+    document.getElementById('overlay2').style.width = "100%";
+    teamA = a_team;
+    teamB = b_team;
+    document.getElementById('ateamlabel').innerHTML = teamA;
+    document.getElementById('bteamlabel').innerHTML = teamB;
+    lineupA = a_lineup;
+    lineupB = b_lineup;
+    
+    document.getElementById('alineup1').value = a_lineup[0];
+    document.getElementById('alineup2').value = a_lineup[1];
+    document.getElementById('alineup3').value = a_lineup[2];
+    document.getElementById('alineup4').value = a_lineup[3];
+    document.getElementById('alineup5').value = a_lineup[4];
+    document.getElementById('alineup6').value = a_lineup[5];
+    document.getElementById('alineupL').value = a_lineup[6];
+    
+    document.getElementById('blineup1').value = b_lineup[0];
+    document.getElementById('blineup2').value = b_lineup[1];
+    document.getElementById('blineup3').value = b_lineup[2];
+    document.getElementById('blineup4').value = b_lineup[3];
+    document.getElementById('blineup5').value = b_lineup[4];
+    document.getElementById('blineup6').value = b_lineup[5];
+    document.getElementById('blineupL').value = b_lineup[6];
 }
 
 function rotate(team)
@@ -122,24 +160,21 @@ function start()
 {
     var saved = '{';
     
-    var aLineup = [];
-    var bLineup = [];
+    lineupA[0] = document.getElementById('alineup1').value;
+    lineupA[1] = document.getElementById('alineup2').value;
+    lineupA[2] = document.getElementById('alineup3').value;
+    lineupA[3] = document.getElementById('alineup4').value;
+    lineupA[4] = document.getElementById('alineup5').value;
+    lineupA[5] = document.getElementById('alineup6').value;
+    lineupA[6] = document.getElementById('alineupL').value;
     
-    aLineup[0] = document.getElementById('alineup1').value;
-    aLineup[1] = document.getElementById('alineup2').value;
-    aLineup[2] = document.getElementById('alineup3').value;
-    aLineup[3] = document.getElementById('alineup4').value;
-    aLineup[4] = document.getElementById('alineup5').value;
-    aLineup[5] = document.getElementById('alineup6').value;
-    aLineup[6] = document.getElementById('alineupL').value;
-    
-    bLineup[0] = document.getElementById('blineup1').value;
-    bLineup[1] = document.getElementById('blineup2').value;
-    bLineup[2] = document.getElementById('blineup3').value;
-    bLineup[3] = document.getElementById('blineup4').value;
-    bLineup[4] = document.getElementById('blineup5').value;
-    bLineup[5] = document.getElementById('blineup6').value;
-    bLineup[6] = document.getElementById('blineupL').value;
+    lineupB[0] = document.getElementById('blineup1').value;
+    lineupB[1] = document.getElementById('blineup2').value;
+    lineupB[2] = document.getElementById('blineup3').value;
+    lineupB[3] = document.getElementById('blineup4').value;
+    lineupB[4] = document.getElementById('blineup5').value;
+    lineupB[5] = document.getElementById('blineup6').value;
+    lineupB[6] = document.getElementById('blineupL').value;
     
     for(i = 0; i < 6; i++)
     {
@@ -158,12 +193,12 @@ function start()
     for(i = 0; i < 6; i++)
         for(j = i + 1; j < 7; j++)
         {
-            if(aLineup[i] == aLineup[j])
+            if(lineupA[i] == lineupA[j])
             {
                 Android.toast('Cannot have repeating number: ' + aLineup[i]);
                 return;
             }
-            if(bLineup[i] == bLineup[j])
+            if(lineupB[i] == lineupB[j])
             {
                 Android.toast('Cannot have repeating number: ' + bLineup[i]);
                 return;
@@ -174,15 +209,15 @@ function start()
     var tBL;
     var tServe;
     
-    if(aLineup[6] == '')
+    if(lineupA[6] == '')
         tAL = '-1';
     else
-        tAL = aLineup[6];
+        tAL = lineupA[6];
     
-    if(bLineup[6] == '')
+    if(lineupB[6] == '')
         tBL = '-1';
     else
-        tBL = bLineup[6];
+        tBL = lineupB[6];
     
     if(document.getElementById('cap').value == '')
         tCap = '-1';
@@ -194,8 +229,8 @@ function start()
     else
         tServe = 'false';
     
-    saved += '"teamA":"' + document.getElementById('teamA').value + '", ';
-    saved += '"teamB":"' + document.getElementById('teamB').value + '", ';
+    saved += '"teamA":"' + teamA + '", ';
+    saved += '"teamB":"' + teamB + '", ';
     saved += '"aWins":0, "bWins":0, ';
     saved += '"sets":' + document.getElementById('sets').value + ', ';
     saved += '"playTo":' + document.getElementById('playTo').value + ', ';
@@ -204,20 +239,20 @@ function start()
     saved += '"aServe":' + tServe + ', ';
     
     saved += '"aLineup":[';
-    saved += aLineup[0] + ", ";
-    saved += aLineup[1] + ", ";
-    saved += aLineup[2] + ", ";
-    saved += aLineup[3] + ", ";
-    saved += aLineup[4] + ", ";
-    saved += aLineup[5] + ", ";
+    saved += lineupA[0] + ", ";
+    saved += lineupA[1] + ", ";
+    saved += lineupA[2] + ", ";
+    saved += lineupA[3] + ", ";
+    saved += lineupA[4] + ", ";
+    saved += lineupA[5] + ", ";
     saved += tAL + '], "bLineup":[';
     
-    saved += bLineup[0] + ", ";
-    saved += bLineup[1] + ", ";
-    saved += bLineup[2] + ", ";
-    saved += bLineup[3] + ", ";
-    saved += bLineup[4] + ", ";
-    saved += bLineup[5] + ", ";
+    saved += lineupB[0] + ", ";
+    saved += lineupB[1] + ", ";
+    saved += lineupB[2] + ", ";
+    saved += lineupB[3] + ", ";
+    saved += lineupB[4] + ", ";
+    saved += lineupB[5] + ", ";
     saved += tBL + ']';
     saved += '}';
 
