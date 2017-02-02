1be3a91aa823a4ad0369d3f1d8051c3ad23f5b3a
Erik <ginshmastc@gmail.com>
2017-02-01 23:49:05 -0800

    Update and rename CIF-setup5.91.js to CIF-setup5.92.js

--- Javascript/additional/CIF-setup5.92.js
+++ Javascript/additional/CIF-setup5.92.js
@@ -110,14 +110,14 @@ function startWithOverlay2(webdata, a_wins, b_wins)
         
         document.getElementById('teamA').hidden = true;
         document.getElementById('teamB').hidden = true;
-        /*
+        
         document.getElementById('teamALabel').innerHTML = 'Final Set: ';
         document.getElementById('teamBLabel').hidden = true;
-        */
+        /*
         document.getElementById('sets1').hidden = true;
         document.getElementById('sets2').hidden = true;
         document.getElementById('sets3').hidden = true;
-        
+        */
         document.getElementById("overlay1").style.width = "100%";
     }
     else
