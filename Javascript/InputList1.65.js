f4f7c153bb9e4e5431f401dd795fa4b2f27c6c27
Erik <erik_holden@comcast.net>
2017-02-14 21:07:06 -0800

    Update and rename InputList1.64.js to InputList1.65.js

--- Javascript/InputList1.65.js
+++ Javascript/InputList1.65.js
@@ -108,14 +108,16 @@ function gameString(startdata)
 {
     initIterator();
     var content = startdata.slice(0, -1) + ', "input":[';
-    
+     Android.test(content);
     var curElement = nextInput();
     while(curElement != null)
     {
+         Android.test(content);
         content += '"' + curElement.data + '", ';
         curElement = nextInput();
     }
     content = content.slice(0, -1) + ']}';
+    Android.test(content);
     return content;
 }
 
@@ -136,6 +138,6 @@ function loadGameData(gamedata)
 function packageDataForAndroid()
 {
     Android.test("creating package...");
-    Android.getPackage(gameString());
+    Android.getPackage(gameString(startingData));
 }
 
