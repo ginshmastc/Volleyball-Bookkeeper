543aa5159b617ce87d3dfc56511fdf03ece9270c
Erik <erik_holden@comcast.net>
2018-02-28 22:41:26 -0800

    Create Mobile_ui_helper.js

---
+++ Javascript/Mobile_ui_helper.js
@@ -0,0 +1,21 @@
+
+
+function getMobileOperatingSystem() {
+  var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+      // Windows Phone must come first because its UA also contains "Android"
+    if (/windows phone/i.test(userAgent)) {
+        return "Windows Phone";
+    }
+
+    if (/android/i.test(userAgent)) {
+        return "Android";
+    }
+
+    if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+        return "iOS";
+    }
+//Use as web interface if not a known operating system
+    return "Web";
+}
+
